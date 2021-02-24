49deacf443e05f6691b94dc493b154952b7e2f84
Shyam Sudhakaran <shyamsnair@protonmail.com>
2021-02-23 18:53:52 -0800

    added some boilerplate

---
+++ evocraft_py/__init__.py
@@ -0,0 +1,8 @@
+from evocraft_py.minecraft_pb2 import * # noqa
+from evocraft_py.minecraft_pb2_grpc import * # noqa
+
+
+"""evocraft-py - Python client for Evocraft"""
+
+__version__ = '0.1.0'
+__all__ = []
