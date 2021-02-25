1dfa23752117a4c17637bb96dd3d65d71bd3ec52
Shyam Sudhakaran <shyamsnair@protonmail.com>
2021-02-24 17:42:49 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="test_evocraft_py",
-    version="version='0.1.5'",
+    version='0.1.5',
     license='MIT',
 
     author="Shyam Sudhakaran",
