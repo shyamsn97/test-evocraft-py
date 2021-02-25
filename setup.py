2015042e6745feab6c3afb3870729ae5e1ec5cf0
Shyam Sudhakaran <shyamsnair@protonmail.com>
2021-02-24 17:37:21 -0800

    Bump version: 0.1.0 → 0.1.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="test_evocraft_py",
-    version="0.1.0",
+    version="version='0.1.4'",
     license='MIT',
 
     author="Shyam Sudhakaran",
--- test_evocraft_py/__init__.py
+++ test_evocraft_py/__init__.py
@@ -4,5 +4,5 @@ from test_evocraft_py.test_evocraft_py import * # noqa
 
 """evocraft-py - Python client for Evocraft"""
 
-__version__ = '0.1.0'
+__version__ = '0.1.4'
 __all__ = []
