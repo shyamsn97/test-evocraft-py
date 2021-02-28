7427020f8d8b74ee0b63c749ca946a27d8ee3430
Shyam Sudhakaran <shyamsnair@protonmail.com>
2021-02-27 23:41:12 -0800

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="test_evocraft_py",
-    version='0.1.6',
+    version='0.1.7',
     license='MIT',
 
     author="Shyam Sudhakaran",
